d13350e74dcfba49662889bdedfcc6c40e7d3078
Mani Kamali <kamalisarvestani.1@themetroschool.org>
2019-01-20 22:52:48 -0500

    Initial commit
    Working website with a form and SQLlite database/CSV downloadage.

---
+++ database.py
@@ -0,0 +1,26 @@
+import csv
+import sqlite3
+
+def create_table():
+    conn = sqlite3.connect('scouting.db')
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''CREATE TABLE scouting (team_number int , sandstorm_rocket_points int , sandstorm_cargoship_points int, starting_level int, teleop_rocket_points int, teleop_cargoship_points int, climb int, other text)''')
+    except Exception as e: print(e)
+
+def submit(team_number, sandstorm_rocket_points, sandstorm_cargoship_points, starting_level, teleop_rocket_points, teleop_cargoship_points, climb, other):
+    conn = sqlite3.connect('scouting.db')
+    cursor = conn.cursor()
+    scouting_info = (int(team_number), int(sandstorm_rocket_points), int(sandstorm_cargoship_points), int(starting_level), int(teleop_rocket_points), int(teleop_cargoship_points), int(climb), other)
+    cursor.execute("INSERT INTO scouting VALUES (?,?,?,?,?,?,?,?)", scouting_info)
+    conn.commit()
+
+def create_csv():
+    conn = sqlite3.connect('scouting.db')
+    cursor = conn.cursor()
+    data = cursor.execute("SELECT * FROM scouting")
+    with open('output.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(('Team Number', 'Sandstorm Rocket Points', 'Sandstorm Cargoship Points', 'Starting Level', 'Teleop Rocket Points', 'Teleop Cargoship Points', 'Climb', 'Other'))
+        writer.writerows(data)
+
---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, send_file
+import database
+
+app = Flask(__name__)
+database.create_table()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/match_scouting', methods=['GET', 'POST'])
+def match_scouting():
+    return render_template('form.html')
+
+@app.route('/submitted_match', methods=['GET', 'POST'])
+def submitted_match():
+    database.submit(team_number=request.form['team'], sandstorm_rocket_points=request.form['sandstorm_rocket_points'], sandstorm_cargoship_points=request.form['sandstorm_cargoship_points'], starting_level=request.form['starting_level'], teleop_rocket_points=request.form['teleop_rocket_points'], teleop_cargoship_points=request.form['teleop_cargoship_points'], climb=request.form['climb'], other=request.form['other'])
+    return render_template('submitted.html')
+
+@app.route('/download_data', methods=['GET', 'POST'])
+def download_csv():
+    database.create_csv()
+    return send_file('output.csv')
+
+@app.route('/graphic.png', methods=['GET', 'POST'])
+def graphic():
+    return send_file('graphic.png')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
