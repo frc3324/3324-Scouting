c1e10533e6f32e2c36c7f0731480851bc4b5b473
Andrew Kingery <kandrew2022@gmail.com>
2019-03-21 09:18:47 -0400

    Changed the port to 80 for Garfielf Integration
    Changed the port to 80 in server.py in order to git pull for garfielf.net

--- server.py
+++ server.py
@@ -123,5 +123,4 @@ def teamInformation():
 def autofill():
     return send_file('autofill.js')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+if __name__ == '__main__': app.run(host='0.0.0.0',port='80')
