d7e56600f1803fd968882a537629471a0748cb15
Walden Hart <hart.2@themetroschool.org>
2020-02-13 00:37:44 +0000

    We added cookies, and added a method that turns the variables in html to JSON format

--- static/js/matchScouting.js
+++ static/js/matchScouting.js
@@ -1,3 +1,5 @@
+boolean cookieCheck;
+
 function increase(ele) {
   var input = ele.parentNode.children[2];
   input.value = parseInt(input.value) + 1;
@@ -18,4 +20,27 @@ function decrease(ele) {
   if (ele.parentNode.children[1].disabled) {
     ele.parentNode.children[1].disabled = false;
   }
+}
+
+// make a cookie if this is the first input this page, then set currentCookie to true and update data
+function bakeCookies() {
+    if (cookieCheck == false)
+	document.cookie = "currentCookie";
+    cookieCheck = true;
+    else {
+	currentCookie = ;
+    }
+	}
+
+// set currentCookie to false so that a new one will be created, and reset page to default values
+function newPage() {
+    
+}
+
+// sets variables values equal to html values in JSON
+function htmlToVar(var var1) {
+
+    var var2 = variablesName(var1) + ": " + var1;
+
+    return var2;
 }
